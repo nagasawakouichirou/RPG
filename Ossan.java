09b4a1f5553bbbe596d57f7476f17a8d46d78403
nagasawakouichirou <46552239+nagasawakouichirou@users.noreply.github.com>
2019-03-05 23:46:59 +0900

    コード変更

--- Ossan.java
+++ Ossan.java
@@ -1,14 +1,17 @@
 
 
-public class Ossans {
+public class Ossan {
 
 	public String name;
 	public int mp;
 	public int hp;
+	public String syoukai;
 	public void introduce() {
 		System.out.println("HP"+hp);
 		System.out.println("mp"+mp);
-		System.out.println("名前は"+name);
+		System.out.println(name);
+		System.out.println(syoukai);
 		System.out.println(" ");
+
 	}
 }
--- RPG.java
+++ RPG.java
@@ -1,17 +1,20 @@
 public class RPG {
 	public static void main(String[] args) {
 
-		Ossans x = new Ossans();
+		Ossan x = new Ossan();
 
-		x.name ="青いおっさんです！";
+		x.name ="青いおっさん";
 		x.hp = 1;
 		x.mp = 3;
+		x.mp = 3;
+		x.syoukai ="レベル1です!" ;
 
-		Ossans x1 = new Ossans();
+		Ossan x1 = new Ossan();
 
-		x1.name ="赤いおっさんです！";
+		x1.name ="赤いおっさん";
 		x1.hp = 7000000;
 		x1.mp = 9000000;
+		x1.syoukai ="レベル12です!" ;
 
 		x.introduce();
 		x1.introduce();
